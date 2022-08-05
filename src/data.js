6193f56dcbcc441007ae0504d3e203ce7f0d8457
ammuanju97 <anjutennyson@gmail.com>
2022-08-05 18:12:23 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://images.unsplash.com/photo-1602143407151-7111542de6e8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    title: "SUMMER SALE",
+    desc: "Water Bottles · Romax SUMMER CHOICE SUPER WATER COOL SQUARE WATER BOTTLE 1000 ml Bottle · Vastate Stainless Steel Hot & Cold Smart Insulated Bottle with LED ..",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://images.unsplash.com/photo-1625708458528-802ec79b1ed8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+    title: "AUTUMN SALE",
+    desc: "Water Bottles · Romax SUMMER CHOICE SUPER WATER COOL SQUARE WATER BOTTLE 1000 ml Bottle · Vastate Stainless Steel Hot & Cold Smart Insulated Bottle with LED .",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://images.unsplash.com/photo-1605714312496-01e90cb509cc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80",
+    title: "WINTER SALE",
+    desc: "Water Bottles · Romax SUMMER CHOICE SUPER WATER COOL SQUARE WATER BOTTLE 1000 ml Bottle · Vastate Stainless Steel Hot & Cold Smart Insulated Bottle with LED .",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://5.imimg.com/data5/EL/RQ/MY-66462571/water-bottles-1000x1000.jpg",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://5.imimg.com/data5/EL/RQ/MY-66462571/water-bottles-1000x1000.jpg",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://5.imimg.com/data5/EL/RQ/MY-66462571/water-bottles-1000x1000.jpg",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+  {
+    id:2,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+  {
+    id:3,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+  {
+    id:4,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+  {
+    id:5,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+  {
+    id:6,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+  {
+    id:7,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+  {
+    id:8,
+    img:"https://5.imimg.com/data5/EP/CX/MY-66462571/61yoldnmtcl-_sl1500_-1000x1000.jpg",
+  },
+]
